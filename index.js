53670dec9570f337013eb22ecdfac8839b7ce22f
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-05-12 11:59:01 -0400

    Saving work for submission

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function createLoginTracker(userInfo) {
   let attemptCount = 0;
 
-  // Inner arrow function will go here
+  const loginAttempt = (passwordAttempt) => {
+    if (attemptCount >= 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    attemptCount++;
+
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
 
   return loginAttempt;
 }
